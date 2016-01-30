8b5da6471625c0f1d92f892fbbe2d9e807cd49fd
Alex Risner <alextrisner@gmail.com>
2016-01-30 12:49:15 -0600

    Get and add acronym functions added
    Was also tested to ensure it worked properly.

--- acrodb.py
+++ acrodb.py
@@ -9,12 +9,37 @@ def close():
     _conn.close()
     print "Connection closed"
 
+def commit():
+    _conn.commit()
+    print "Committed local changes"
+
+def getAcronym(acronym):
+    results = []
+    cursor = _conn.execute("SELECT * from ACRONYMS WHERE ACRONYM = '%s'" % acronym.upper())
+    for row in cursor:
+        row_dict = {'Acronym' : row[0], 'Description' : row[1]}
+        results.append(row_dict)
+    print("Found %s results in local database" % len(results))
+    return results
+
+def addAcronyms(acronyms):
+    added = 0
+    for row_dict in acronyms:
+        try:
+            _conn.execute("INSERT INTO ACRONYMS (ACRONYM,DESCRIPTION) VALUES ('%s', '%s')" % (row_dict['Acronym'].upper(), row_dict['Description']))
+            print "Added a new acronym and description to local database"
+            added += 1
+        except sqlite3.IntegrityError:
+            print "Did not add new acronym since it was already in table"
+    print("Added %s new acronyms to database" % str(added))
+    commit()
+
 if __name__ == "__main__":
     try:
         _conn.execute('''CREATE TABLE ACRONYMS
-            (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            ACRONYM TEXT NOT NULL,
-            DESCRIPTION TEXT NOT NULL);''')
+            (ACRONYM TEXT NOT NULL,
+            DESCRIPTION TEXT NOT NULL,
+            PRIMARY KEY (ACRONYM, DESCRIPTION));''')
         print "Acronym table created successfully"
     except sqlite3.OperationalError:
         print "Table already exists, did not create table"
