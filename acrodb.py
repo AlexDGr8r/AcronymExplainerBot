9ccdd3b11ce8cfa3f85c02556c9199dc01c9790e
Alex Risner <alextrisner@gmail.com>
2016-01-31 14:30:46 -0600

    Abbreviations.com API implemented
    Lots of changes besides the API:
    Now securely using sqlite (no SQL injection). Restructured comment
    parsing to use tables instead of dividers.
    Popularity table added and implemented

--- acrodb.py
+++ acrodb.py
@@ -15,24 +15,57 @@ def commit():
 
 def getAcronym(acronym):
     results = []
-    cursor = _conn.execute("SELECT * from ACRONYMS WHERE ACRONYM = '%s'" % acronym.upper())
+    args = (acronym.upper(),)
+    cursor = _conn.execute("SELECT * from ACRONYMS WHERE ACRONYM=?", args)
     for row in cursor:
-        row_dict = {'Acronym' : row[0], 'Description' : row[1]}
-        results.append(row_dict)
+        results.append(row[1])
     print("Found %s results in local database" % len(results))
     return results
 
-def addAcronyms(acronyms):
+def addAcronyms(acronym, descriptions, more_results):
     added = 0
-    for row_dict in acronyms:
+    for desc in descriptions:
+        if added == 10:
+            break
         try:
-            _conn.execute("INSERT INTO ACRONYMS (ACRONYM,DESCRIPTION) VALUES ('%s', '%s')" % (row_dict['Acronym'].upper(), row_dict['Description']))
+            args = (acronym.upper(), desc)
+            _conn.execute("INSERT INTO ACRONYMS (ACRONYM,DESCRIPTION) VALUES (?, ?)", args)
             print "Added a new acronym and description to local database"
             added += 1
         except sqlite3.IntegrityError:
             print "Did not add new acronym since it was already in table"
+
+    try:
+        more = 0
+        if more_results == True:
+            more = 1
+        args = (acronym.upper(), 1, more)
+        _conn.execute("INSERT INTO POPULARITY VALUES (?, ?, ?);", args)
+    except sqlite3.IntegrityError:
+        print "Did not add new acronym to Popularity table since it was already in table"
+
     print("Added %s new acronyms to database" % str(added))
     commit()
+
+# Returns hits and if more results are on abbreviations.com (hits, more)
+def getPopularity(acronym):
+    args = (acronym.upper(),)
+    cursor = _conn.execute("SELECT * from POPULARITY WHERE ACRONYM=?", args)
+    row = cursor.fetchone()
+    more = False
+    print("row[2]=%d" % row[2])
+    if int(row[2]) == 1:
+        print "Set to true"
+        more = True
+    return row[1], more
+
+def increasePopularity(acronym):
+    views, more = getPopularity(acronym)
+    views += 1
+    args = (views, acronym.upper())
+    cursor = _conn.execute("UPDATE POPULARITY set HITS=? WHERE ACRONYM=?", args)
+    commit()
+    print "Popularity of %s updated to %d" % (acronym.upper(), views)
 
 if __name__ == "__main__":
     try:
@@ -42,5 +75,13 @@ if __name__ == "__main__":
             PRIMARY KEY (ACRONYM, DESCRIPTION));''')
         print "Acronym table created successfully"
     except sqlite3.OperationalError:
-        print "Table already exists, did not create table"
+        print "Acronym Table already exists, did not create table"
+    try:
+        _conn.execute('''CREATE TABLE POPULARITY
+            (ACRONYM TEXT PRIMARY KEY NOT NULL,
+            HITS INTEGER NOT NULL,
+            MORE_RESULTS BOOLEAN NOT NULL);''')
+        print "Popularity table created successfully"
+    except sqlite3.OperationalError:
+        print "Popularity Table already exists, did not create table"
     close()
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import json
 import re
 import praw
 import OAuth2Util
+import urllib2
+import xmltodict
 import acrodb
 from collections import deque
 
@@ -30,12 +32,44 @@ def save_cache(filename):
     f.close()
 
 def reply_to_comment(comment, acronym, acronyms):
-    text = "For those that do not understand what **%s** stands for, I'm here to help!\n\n### Explanation(s):\n\n***\n\n" % acronym
-    for acro_dict in acronyms:
-        text += "> %s\n\n***\n\n" % acro_dict['Description']
-    text += "*I am simply a bot. If the information I've displayed is incorrect, please message my creator /u/AlexDGr8r.*"
-    comment.reply(text)
+    num_described = 0
+    views, more = acrodb.getPopularity(acronym)
+    text = "For those that do not understand what **%s** stands for, I'm here to help!\n\nDescription for %s |\n-------- |\n" % (acronym, acronym)
+    for description in acronyms:
+        if num_described == 10:
+            break
+        text += "%s |\n" % description
+        num_described += 1
+    if more == True:
+        text += "**There are even more explanations on [abbreviations.com](http://www.abbreviations.com/). Check them out [here!](http://www.abbreviations.com/%s)**\n" % acronym.upper()
+    if views > 1:
+        text += "\n**This acronym has been requested %d times.**\n" % views
+    else:
+        text += "\n**This is the first time this acronym has been requested.**\n"
+    text += "***\n^(I am simply a bot. If the information I've displayed is incorrect, please message my creator /u/AlexDGr8r.)"
+    try:
+        comment.reply(text)
+    except praw.errors.RateLimitExceeded as error:
+        print("Sleeping for %d seconds due to exceeding rate limit" % error.sleep_time)
+        time.sleep(error.sleep_time)
+        comment.reply(text)
     print('Replied to a comment')
+
+def getDescriptionsFromAPI(acronym, uid, token):
+    xmlfile = urllib2.urlopen('http://www.stands4.com/services/v2/abbr.php?uid=%s&tokenid=%s&term=%s' % (uid, token, acronym.upper()))
+    data = xmlfile.read()
+    xmlfile.close()
+    jsondata = xmltodict.parse(data)
+    results = jsondata['results']
+    terms = results['result']
+    descriptions = []
+    for term in terms:
+        desc = term['definition']
+        if desc in descriptions:
+            print('Duplicate defintion, ignoring')
+            continue
+        descriptions.append(desc)
+    return descriptions
 
 ###################### Begin Script ######################
 
@@ -45,6 +79,12 @@ with open('config.json') as data_file:
 
 # Load the cache
 cache = load_cache(CACHE_FILE_NAME)
+
+# Abbreviations.com API credentials
+API_UID = config['API_UID']
+print('UID=%s' % API_UID)
+API_TOKEN = config['API_Token']
+print('token=%s' % API_TOKEN)
 
 # Compile subreddits we want to pull the comments from
 subreddits = ''
@@ -72,13 +112,19 @@ try:
 
             match = re.match(r'!acro(nym)?s?bot (\w+)', comment.body.lstrip(), re.I)
             if match:
-                acro = match.group(2).upper() # the acronym is subgroup 2
+                acro = match.group(2).upper()   # the acronym is subgroup 2
                 local_results = acrodb.getAcronym(acro)
-                if len(local_results) > 0:
+                if len(local_results) > 0:      # Found in local database, no need to use API
+                    acrodb.increasePopularity(acro)
                     reply_to_comment(comment, acro, local_results)
                 else:
-                    print("Not in local database. Ignoring for now")
-                    # TODO check abbreviations.com API if not in local database
+                    descriptions = getDescriptionsFromAPI(acro, API_UID, API_TOKEN)
+                    if len(descriptions) > 0:
+                        more = False
+                        if len(descriptions) > 10:
+                            more = True
+                        acrodb.addAcronyms(acro, descriptions, more)
+                        reply_to_comment(comment, acro, descriptions)
 
         print 'Going to sleep'
         time.sleep(10)
