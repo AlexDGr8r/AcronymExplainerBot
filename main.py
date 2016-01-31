c0c99b75a2d854b729e45138317141ace0a5c4aa
Alex Risner <alextrisner@gmail.com>
2016-01-30 18:40:44 -0600

    Replies properly integrated, fixed loading cache
    Replies to comments now are properly formatted and use local sqlite
    database. Fixed cache not loading from file properly.

--- main.py
+++ main.py
@@ -2,25 +2,26 @@
 
 import time
 import json
+import re
 import praw
 import OAuth2Util
+import acrodb
 from collections import deque
-from pprint import pprint
 
 USER_AGENT = "PRAW:Acronym Explainer:v0.1 by /u/AlexDGr8r"
 CACHE_FILE_NAME = 'cache.dat'
 
 r = praw.Reddit(user_agent=USER_AGENT)
 o = OAuth2Util.OAuth2Util(r)
-cache = deque(maxlen=200)
 
 def load_cache(filename):
     try:
-        with open(filename) as f:
-            cache = deque(f.read().splitlines(), maxlen=200)
+        f = open(filename, 'r')
+        lines = f.read().splitlines();
+        return deque(lines, maxlen=200)
     except IOError:
-        cache = deque(maxlen=200)
         print('%s does not exist. Ignoring.' % filename)
+        return deque(maxlen=200)
 
 def save_cache(filename):
     f = open(filename, 'w')
@@ -28,20 +29,22 @@ def save_cache(filename):
         f.write('%s\n' % item)
     f.close()
 
-# TODO rework for database
-def reply_to_comment(comment, acronym):
-    text = "For those that do not understand what %s stands for, I'm here to help!\n\n### Explanation:\n\n" % acronym
-    text += acronyms[acronym] + "\n\n*****\n\n"
-    text += "*I am simply a bot. If the information I've displayed is incorrect, please message my creator /u/AlexDGr8r*"
+def reply_to_comment(comment, acronym, acronyms):
+    text = "For those that do not understand what **%s** stands for, I'm here to help!\n\n### Explanation(s):\n\n***\n\n" % acronym
+    for acro_dict in acronyms:
+        text += "> %s\n\n***\n\n" % acro_dict['Description']
+    text += "*I am simply a bot. If the information I've displayed is incorrect, please message my creator /u/AlexDGr8r.*"
     comment.reply(text)
     print('Replied to a comment')
+
+###################### Begin Script ######################
 
 # Load JSON data from config.json
 with open('config.json') as data_file:
     config = json.load(data_file)
 
 # Load the cache
-load_cache(CACHE_FILE_NAME)
+cache = load_cache(CACHE_FILE_NAME)
 
 # Compile subreddits we want to pull the comments from
 subreddits = ''
@@ -59,16 +62,28 @@ try:
             if comment.id in cache:
                 print('Breaking...')
                 break
+
+            cache.append(comment.id)
+            print('appended to cache')
+
             if comment.author.name == "AcronymExplainerBot":
                 print('ignoring my own comment')
                 continue
-            cache.append(comment.id)
-            print('appended to cache')
-            # TODO USE sqlite db instead of json
+
+            match = re.match(r'!acro(nym)?s?bot (\w+)', comment.body.lstrip(), re.I)
+            if match:
+                acro = match.group(2).upper() # the acronym is subgroup 2
+                local_results = acrodb.getAcronym(acro)
+                if len(local_results) > 0:
+                    reply_to_comment(comment, acro, local_results)
+                else:
+                    print("Not in local database. Ignoring for now")
+                    # TODO check abbreviations.com API if not in local database
 
         print 'Going to sleep'
-        time.sleep(2)
+        time.sleep(10)
 except (KeyboardInterrupt, SystemExit):
     save_cache(CACHE_FILE_NAME)
     print('Saved cache')
+    acrodb.close()
     print('exiting')
